1141c15094fee28df290493adb0d2ef248b22961
Naveed123-ux <naveedzafar123abx@gmail.com>
2026-02-26 22:33:03 +0500

    first commit

---
+++ packages/medusae/src/defaults.js
@@ -0,0 +1,40 @@
+const MEDUSAE_DEFAULTS = {
+  cursor: {
+    radius: 0.065,
+    strength: 3.4,
+    dragFactor: 0.02,
+  },
+  halo: {
+    outerOscFrequency: 1.85,
+    outerOscAmplitude: 0.92,
+    outerOscJitterStrength: 0.12,
+    outerOscJitterSpeed: 0.85,
+    radiusBase: 2.4,
+    radiusAmplitude: 0.62,
+    shapeAmplitude: 0.82,
+    rimWidth: 2.05,
+    outerStartOffset: 0.4,
+    outerEndOffset: 2.2,
+    scaleX: 1.3,
+    scaleY: 1,
+  },
+  particles: {
+    baseSize: 0.016,
+    activeSize: 0.044,
+    blobScaleX: 1,
+    blobScaleY: 0.6,
+    rotationSpeed: 0.13,
+    rotationJitter: 0.24,
+    cursorFollowStrength: 1.15,
+    oscillationFactor: 1.15,
+    colorBase: "#0000ff",
+    colorOne: "#4285f5",
+    colorTwo: "#eb4236",
+    colorThree: "#faba03",
+  },
+  background: {
+    color: "#ffffff",
+  },
+};
+
+export default MEDUSAE_DEFAULTS;
